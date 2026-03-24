15a054e18f242f66c41cd8cdd8f80527e3ad81bf
abubuh <cristian_caht@hotmail.com>
2026-03-24 18:07:15 -0500

    Cors url

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://tu-app.vercel.app",
+    origin: "https://jwt-practice-git-main-abubuhs-projects.vercel.app",
   }),
 );
 app.get("/", (req, res) => {
