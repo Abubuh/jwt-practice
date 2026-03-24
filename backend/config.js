0d3d74b9628480534a04642967cffecca3278434
abubuh <cristian_caht@hotmail.com>
2026-03-24 17:20:06 -0500

    Add config

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = process.env.PORT ?? 3000;
+export const SALT_ROUNDS = process.env.SALT_ROUNDS ?? 10;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const DATABASE_URL = process.env.DATABASE_URL;
